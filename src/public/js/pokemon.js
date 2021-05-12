930b9a16774ebe7b7da612cc74c34a442b787d06
Barrot Francisco <franciscobarrot98@gmail.com>
2021-05-12 11:30:27 -0300

    cambio de plantilla, estilos base, ordenar los pokemon

--- src/public/js/pokemon.js
+++ src/public/js/pokemon.js
@@ -2,28 +2,38 @@ function stateChangeHandler() {
   if (this.readyState === 4 && this.status === 200) {
     let res = JSON.parse(this.responseText);
     let results = res.results;
-    
-    results.forEach(pok => makeRequest(pok.url, setPokemon));
-   
+
+    pokemonImages.forEach((pokemon, index) => {
+      pokemon.id = results[index].name;
+    });
+
+    results.forEach((pok) => makeRequest(pok.url, setPokemon));
   }
 }
 
 function setPokemon() {
   if (this.readyState === 4 && this.status === 200) {
     let res = JSON.parse(this.responseText);
-    
-    let img = document.createElement("img");
-    img.src = res.sprites.other["official-artwork"].front_default;
-    img.classList.add("image");
-    document.body.appendChild(img);
+    pokemonImages.forEach((container) => {
+      if (container.id == res.name) {
+        container.style.backgroundImage = `url(${res.sprites.other['official-artwork'].front_default})`;
+        const h5 = document.createElement('h5');
+        h5.textContent = res.name[0].toUpperCase() + res.name.slice(1);
+        container.appendChild(h5);
+      }
+    });
   }
 }
 
 function makeRequest(url, func) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = func;
-  xhttp.open("GET", url, true);
+  xhttp.open('GET', url, true);
   xhttp.send();
 }
 
-makeRequest("https://pokeapi.co/api/v2/pokemon?limit=3&offset=400", stateChangeHandler);
+makeRequest(
+  'https://pokeapi.co/api/v2/pokemon?limit=6&offset=0',
+  stateChangeHandler
+);
+const pokemonImages = document.querySelectorAll('.pokemon');
