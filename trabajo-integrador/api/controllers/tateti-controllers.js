677dc8eecded35e8cbf91094cfbed43daada9781
Barrot Francisco <franciscobarrot98@gmail.com>
2021-06-19 19:18:02 -0300

    checkWinner added and agregamos winner field

--- trabajo-integrador/api/controllers/tateti-controllers.js
+++ trabajo-integrador/api/controllers/tateti-controllers.js
@@ -8,6 +8,31 @@ const isValidMove = (board, pos) => {
   return board[pos] === null;
 };
 
+const checkWinner = (board, turn) => {
+  let isWinner = false;
+  for (let i = 0; i < 3; i++) {
+    if (turn === board[i] && turn === board[i + 3] && turn === board[i + 6]) {
+      isWinner = true;
+      break;
+    }
+    if (
+      turn === board[3 * i] &&
+      turn === board[3 * i + 1] &&
+      turn === board[3 * i + 2]
+    ) {
+      isWinner = true;
+      break;
+    }
+  }
+  if (
+    (turn === board[0] && turn === board[4] && turn === board[8]) ||
+    (turn === board[2] && turn === board[4] && turn === board[6])
+  ) {
+    isWinner = true;
+  }
+  return isWinner;
+};
+
 const initializeGame = (req, res) => {
   let currentBoard;
 
@@ -18,6 +43,8 @@ const initializeGame = (req, res) => {
       id: Math.floor(Math.random() * 1000).toString(), //uuid
       board: [null, null, null, null, null, null, null, null, null],
       turn: 'X', // posiblemente debe hacer una id para cada jugador
+      //posible atributo: invalidMove : true/false
+      winner: null,
     };
 
     games.push(currentBoard);
@@ -33,14 +60,21 @@ const updateGame = (req, res) => {
   let currentBoard = searchBoard(req.query.id);
 
   // validar jugada
-  if (isValidMove(currentBoard.board, req.query.square)) {
+  if (isValidMove(currentBoard.board, req.query.square, currentBoard.winner)) {
+    //salta un error del board
     currentBoard.board[req.query.square] = currentBoard.turn;
+    if (checkWinner(currentBoard.board, currentBoard.turn)) {
+      currentBoard.winner = currentBoard.turn;
+      //borrar board luego de haber terminado (hacer un pop)
+    }
     currentBoard.turn = currentBoard.turn === 'X' ? 'O' : 'X';
   }
-  console.log(currentBoard);
+  console.log(games);
 };
 
 module.exports = {
   initializeGame,
   updateGame,
 };
+
+//fix maximo 5 jugadas?
